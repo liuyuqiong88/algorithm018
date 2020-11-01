e5aa606651f773bdd24cc268ce4a0228e84c7987
liuyuqiong88 <36597561+liuyuqiong88@users.noreply.github.com>
2020-11-01 23:20:04 +0800

    Create Q94_binary_tree_inorder_traversal.java

---
+++ week02/Q94_binary_tree_inorder_traversal.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+     TreeNode left;
+     TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+        this.right = right;
+     }
+ }
+
+public class Q94_binary_tree_inorder_traversal {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        inorder(root, res);
+        return res;
+    }
+    public void inorder(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, res);
+        res.add(root.val);
+        inorder(root.right, res);
+    }
+}
